3296e2532d804e6d7313230869162bdfc8ad773f
githubdoesntgivemeausername <aakashsukumar@gmail.com>
2022-10-15 13:20:27 +0530

    updating file with routing modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,24 +11,7 @@ import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    {
-      path: 'recipes',
-      component: RecipesComponent,
-      children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        {
-          path: ':id',
-          component: RecipeDetailComponent,
-          resolve: [RecipesResolverService]
-        },
-        {
-          path: ':id/edit',
-          component: RecipeEditComponent,
-          resolve: [RecipesResolverService]
-        }
-      ]
-    },
+    ,
     { path: 'shopping-list', component: ShoppingListComponent }
   ];
   
